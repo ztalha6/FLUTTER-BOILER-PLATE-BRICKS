2559d2291b65f36a5925d30f7aa36e218552a854
Muhammad Talha Siddique <ztalha6@gmail.com>
2023-10-29 15:06:44 +0500

    User manager updated
    - Basic Implementation added

--- bricks/basic_setup/__brick__/app/data/services/user_manager.dart
+++ bricks/basic_setup/__brick__/app/data/services/user_manager.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../models/user_model.dart';
 
 abstract class IUserRepository {
   Future<bool> saveUser(User user);
   Future<bool> updateUser(User user);
-  Future<User> getUser();
+  Future<User?> getUser();
 }
 
 class UserManager implements IUserRepository {
@@ -17,20 +21,32 @@ class UserManager implements IUserRepository {
   static const String _keyUser = '_keyUser';
 
   @override
-  Future<User> getUser() {
-    // TODO: implement getUser
-    throw UnimplementedError();
+  Future<User?> getUser() async {
+    final String savedUserString =
+        (await SharedPreferences.getInstance()).getString(_keyUser) ?? '';
+    return savedUserString.isNotEmpty
+        ? User.fromJson(json.decode(savedUserString))
+        : null;
   }
 
   @override
-  Future<bool> saveUser(User user) {
-    // TODO: implement saveUser
-    throw UnimplementedError();
+  Future<bool> saveUser(User user) async {
+    return (await SharedPreferences.getInstance()).setString(
+      _keyUser,
+      json.encode(user.toJson()),
+    );
   }
 
   @override
-  Future<bool> updateUser(User user) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<bool> updateUser(User newUser) async {
+    final User? user = await getUser();
+    if (user != null) {
+      if (newUser.name != null) {
+        user.name = newUser.name;
+      }
+      saveUser(user);
+      return true;
+    }
+    return false;
   }
 }
